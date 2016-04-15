1735917b248bf1d38f920f193fa750c3853382d5
Sergey Panfilov <sp@arex.io>
2016-04-15 10:28:46 +0800

    Fixes

--- build/tasks/webserver.js
+++ build/tasks/webserver.js
@@ -6,19 +6,19 @@ const config = require('../config');
 
 gulp.task('webserver', function () {
 
-    gulp.src(['../angular-jsvat/dist', './'])
-        .pipe(webServer({
-            fallback: 'index.html',
-            port: 8001,
-            livereload: true,
-            //directoryListing: true,
-            //proxies: [{
-            //    source: '/register',
-            //    target: 'http://localhost:8080/1337'
-            //}],
-            open: true
-            //middleware: function () {
-            //    return [cors];
-            //}
-        }));
+  gulp.src(['angular-jsvat/dist', '../angular-jsvat/dist', '../../angular-jsvat/dist', '../../../angular-jsvat/dist', './'])
+      .pipe(webServer({
+        fallback: 'index.html',
+        port: 8001,
+        livereload: true,
+        //directoryListing: true,
+        //proxies: [{
+        //    source: '/register',
+        //    target: 'http://localhost:8080/1337'
+        //}],
+        open: true
+        //middleware: function () {
+        //    return [cors];
+        //}
+      }));
 });
