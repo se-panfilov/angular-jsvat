414960429a4b8e79cba8ee2caab0cf3d06118fdd
Sergey Panfilov <sp@arex.io>
2016-04-18 18:13:05 +0800

    Fixes

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 angular.module('demo', ['angular-jsvat'])
 
     .controller('DemoPageCtrl', function ($scope) {
-      $scope.model = {};
+      $scope.model = {
+        value: 'ro43'
+      };
+
+      $scope.$watch('model', function(val, oldVal){
+        console.log(val);
+      });
     })
 ;
