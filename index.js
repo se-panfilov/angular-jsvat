3c8b336d62ad041a399caf549a691554cb53c7cf
Sergey Panfilov <sp@arex.io>
2016-04-20 10:52:03 +0800

    fixes

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 angular.module('demo', ['angular-jsvat'])
 
     .controller('DemoPageCtrl', function ($scope) {
-      $scope.model = {
-        value: 'ro43'
+
+      $scope.models = {
+        common: null,
+        linked: null,
+        predefinedStr: 'CY00001067Y',
+        predefinedEmptyStr: 'CY00001067Y',
+        predefinedObj: {value: 'EE100183238'},
+        predefinedEmptyObj: {value: ''},
+        predefinedNumberObj: 123
       };
 
-      $scope.$watch('model', function(val, oldVal){
-        console.log(val);
-      });
     })
 ;
