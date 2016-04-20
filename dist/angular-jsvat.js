2f9464c3726d8755d4b55fa17c0644288af95dd7
Sergey Panfilov <sp@arex.io>
2016-04-21 02:39:48 +0800

    fixes

--- dist/angular-jsvat.js
+++ dist/angular-jsvat.js
@@ -1,3 +1,58 @@
+'use strict';
+
+angular.module('angular-jsvat-input', [])
+
+    .directive('jsvatInput', ['$compile', 'JsVatFactory', function ($compile, JsVatFactory) {
+      return {
+        restrict: 'E',//TODO (S.Panfilov) Perhaps shuld be "A" restriction
+        replace: true,
+        templateUrl: 'jsvat_input.html',
+        scope: {
+          jsvatModel: '='
+        },
+        link: function (scope, element) {
+          var invalid = '-invalid';
+          var valid = '-valid';
+
+          scope.classObj = {};
+
+          if (!angular.isObject(scope.jsvatModel)) {
+            var value = scope.jsvatModel;
+            scope.jsvatModel = {
+              value: value
+            }
+          }
+
+          function setValidity(isValid) {
+            scope.classObj[invalid] = !isValid;
+            scope.classObj[valid] = isValid;
+            modelController.$setValidity('vat', isValid);
+          }
+
+          var modelController = element.controller('ngModel');
+
+          scope.checkVAT = function () {
+            var result = JsVatFactory.checkVAT(scope.jsvatModel.value, true);
+            scope.jsvatModel.isValid = result.isValid;
+            scope.jsvatModel.countries = result.countries;
+            var isEmpty = scope.jsvatModel.value === '' || (!scope.jsvatModel.value && scope.jsvatModel.value !== '0');
+            setValidity(result.isValid || isEmpty);
+          };
+
+          scope.$watch('jsvatModel.value', function () {
+            scope.checkVAT();
+          });
+
+
+          if (scope.jsvatModel) {
+            scope.checkVAT();
+          }
+
+        }
+      }
+    }])
+;
+angular.module("angular-jsvat.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("jsvat_input.html","<input type=\"text\" ng-model=\"jsvatModel.value\" ng-class=\"classObj\" class=\"jsvat-input__field\"/>");}]);
 angular.module('angular-jsvat', ['angular-jsvat.templates', 'angular-jsvat-input'])
 
 .factory('JsVatFactory', function() {
@@ -1611,119 +1666,3 @@ angular.module('angular-jsvat', ['angular-jsvat.templates', 'angular-jsvat-input
 
   return jsvat;
 });
-'use strict';
-
-angular.module('angular-jsvat-input', [])
-
-    .directive('jsvatInput', ['$compile', 'JsVatFactory', function ($compile, JsVatFactory) {
-      return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'jsvat_input.html',
-        scope: {
-          jsvatModel: '=',
-          jsvatLbl: '?@'
-        },
-        compile: function (tElement, tAttrs) {
-          var label = angular.element(tElement);
-          var input = label[0].children[0];
-
-          //label.removeAttr('ng-model');
-
-          function makeLoverCamelCase(str) {
-            var toUpperCase = function (a) {
-              return a.toUpperCase();
-            };
-            var toLowerCase = function (a) {
-              return a.toLowerCase();
-            };
-            var regex = /(?:^|\s)\S/g;
-
-            function makeUpperCase(s) {
-              return s.replace(regex, toUpperCase);
-            }
-
-            return str.split(/\-/).map(makeUpperCase).join('').replace(regex, toLowerCase);
-          }
-
-          function _moveAttrToInput(attr, val, attrNameAfter) {
-            if (tAttrs.hasOwnProperty(makeLoverCamelCase(attr))) {
-              //label.removeAttr(attr);
-              input.setAttribute(attrNameAfter || attr, val || tAttrs[makeLoverCamelCase(attr)]);
-            }
-          }
-
-          var attrList = [
-            'name',
-            'required',
-            'ng-required',
-            'disabled',
-            'ng-disabled',
-            'readonly',
-            'ng-readonly',
-            'minlength',
-            'ng-minlength',
-            'maxlength',
-            'ng-maxlength',
-            'size',
-            'ng-size'
-          ];
-
-          for (var i = 0; i < attrList.length; i++) {
-            _moveAttrToInput(attrList[i]);
-          }
-
-          _moveAttrToInput('jsvat-input-class', 'jsvatInputClassObj', 'ng-class');
-
-          return {
-            post: function (scope, elem) {
-              $compile(elem)(scope);
-            }
-          };
-        },
-        link: function (scope, element) {
-          var invalid = '-invalid';
-          var valid = '-valid';
-
-          scope.jsvatInputClassObj = {};
-          scope.jsvatLabelClassObj = {};
-
-          if (!angular.isObject(scope.jsvatModel)) {
-            var value = scope.jsvatModel;
-            scope.jsvatModel = {
-              value: value
-            }
-          }
-
-          function setValidity(isValid) {
-            scope.jsvatInputClassObj[invalid] = !isValid;
-            scope.jsvatLabelClassObj[invalid] = !isValid;
-            scope.jsvatInputClassObj[valid] = isValid;
-            scope.jsvatLabelClassObj[valid] = isValid;
-            modelController.$setValidity('vat', isValid);
-          }
-
-          var modelController = element.find('input').controller('ngModel');
-
-          scope.checkVAT = function () {
-            var result = JsVatFactory.checkVAT(scope.jsvatModel.value, true);
-            scope.jsvatModel.isValid = result.isValid;
-            scope.jsvatModel.countries = result.countries;
-            var isEmpty = scope.jsvatModel.value === '' || (!scope.jsvatModel.value && scope.jsvatModel.value !== '0');
-            setValidity(result.isValid || isEmpty);
-          };
-
-          scope.$watch('jsvatModel.value', function () {
-            scope.checkVAT();
-          });
-
-
-          if (scope.jsvatModel) {
-            scope.checkVAT();
-          }
-
-        }
-      }
-    }])
-;
-angular.module("angular-jsvat.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("jsvat_input.html","<label ng-class=\"jsvatLabelClassObj\" class=\"jsvat-input__block\"><input type=\"text\" ng-model=\"jsvatModel.value\" ng-class=\"jsvatInputClassObj\" class=\"jsvat-input__field\"/></label>");}]);
--- src/jsvat-input/jsvat_input.js
+++ src/jsvat-input/jsvat_input.js
@@ -4,76 +4,17 @@ angular.module('angular-jsvat-input', [])
 
     .directive('jsvatInput', ['$compile', 'JsVatFactory', function ($compile, JsVatFactory) {
       return {
-        restrict: 'E',
+        restrict: 'E',//TODO (S.Panfilov) Perhaps shuld be "A" restriction
         replace: true,
         templateUrl: 'jsvat_input.html',
         scope: {
-          jsvatModel: '=',
-          jsvatLbl: '?='
-        },
-        compile: function (tElement, tAttrs) {
-          var label = angular.element(tElement);
-          var input = label[0].children[0];
-
-          //label.removeAttr('ng-model');
-
-          function makeLoverCamelCase(str) {
-            var toUpperCase = function (a) {
-              return a.toUpperCase();
-            };
-            var toLowerCase = function (a) {
-              return a.toLowerCase();
-            };
-            var regex = /(?:^|\s)\S/g;
-
-            function makeUpperCase(s) {
-              return s.replace(regex, toUpperCase);
-            }
-
-            return str.split(/\-/).map(makeUpperCase).join('').replace(regex, toLowerCase);
-          }
-
-          function _moveAttrToInput(attr, val, attrNameAfter) {
-            if (tAttrs.hasOwnProperty(makeLoverCamelCase(attr))) {
-              //label.removeAttr(attr);
-              input.setAttribute(attrNameAfter || attr, val || tAttrs[makeLoverCamelCase(attr)]);
-            }
-          }
-
-          var attrList = [
-            'name',
-            'required',
-            'ng-required',
-            'disabled',
-            'ng-disabled',
-            'readonly',
-            'ng-readonly',
-            'minlength',
-            'ng-minlength',
-            'maxlength',
-            'ng-maxlength',
-            'size',
-            'ng-size'
-          ];
-
-          for (var i = 0; i < attrList.length; i++) {
-            _moveAttrToInput(attrList[i]);
-          }
-
-          _moveAttrToInput('jsvat-input-class', 'jsvatInputClassObj', 'ng-class');
-
-          return {
-            post: function (scope, elem) {
-              $compile(elem)(scope);
-            }
-          };
+          jsvatModel: '='
         },
         link: function (scope, element) {
           var invalid = '-invalid';
           var valid = '-valid';
 
-          scope.jsvatInputClassObj = {};
-          scope.jsvatLabelClassObj = {};
+          scope.classObj = {};
 
           if (!angular.isObject(scope.jsvatModel)) {
             var value = scope.jsvatModel;
@@ -83,14 +24,12 @@ angular.module('angular-jsvat-input', [])
           }
 
           function setValidity(isValid) {
-            scope.jsvatInputClassObj[invalid] = !isValid;
-            scope.jsvatLabelClassObj[invalid] = !isValid;
-            scope.jsvatInputClassObj[valid] = isValid;
-            scope.jsvatLabelClassObj[valid] = isValid;
+            scope.classObj[invalid] = !isValid;
+            scope.classObj[valid] = isValid;
             modelController.$setValidity('vat', isValid);
           }
 
-          var modelController = element.find('input').controller('ngModel');
+          var modelController = element.controller('ngModel');
 
           scope.checkVAT = function () {
             var result = JsVatFactory.checkVAT(scope.jsvatModel.value, true);
