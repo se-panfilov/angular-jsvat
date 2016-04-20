a84388dc68524e3151f20d98f7e1218efe3a3fca
Sergey Panfilov <sp@arex.io>
2016-04-21 02:35:34 +0800

    fixes

--- src/jsvat-input/jsvat_input.js
+++ src/jsvat-input/jsvat_input.js
@@ -8,58 +8,67 @@ angular.module('angular-jsvat-input', [])
         replace: true,
         templateUrl: 'jsvat_input.html',
         scope: {
-          jsvatModel: '='
+          jsvatModel: '=',
+          jsvatLbl: '?='
         },
-        // compile: function (tElement, tAttrs) {
-        //   var label = angular.element(tElement);
-        //   var input = label[0].children[0];
-        //
-        //   //label.removeAttr('ng-model');
-        //
-        //   function makeLoverCamelCase(str) {
-        //     var toUpperCase = function (a) {
-        //       return a.toUpperCase();
-        //     };
-        //     var toLowerCase = function (a) {
-        //       return a.toLowerCase();
-        //     };
-        //     var regex = /(?:^|\s)\S/g;
-        //
-        //     function makeUpperCase(s) {
-        //       return s.replace(regex, toUpperCase);
-        //     }
-        //
-        //     return str.split(/\-/).map(makeUpperCase).join('').replace(regex, toLowerCase);
-        //   }
-        //
-        //   function _moveAttrToInput(attr, val, attrNameAfter) {
-        //     if (tAttrs[makeLoverCamelCase(attr)]) {
-        //       label.removeAttr(attr);
-        //       input.setAttribute(attrNameAfter || attr, val);
-        //     }
-        //   }
-        //
-        //   _moveAttrToInput('jsvat-input-class', 'jsvatInputClassObj', 'ng-class');
-        //   _moveAttrToInput('ng-required', 'opts.isRequired');
-        //   _moveAttrToInput('ng-disabled', 'opts.isDisabled');
-        //   _moveAttrToInput('ng-readonly', 'opts.isReadonly');
-        //   _moveAttrToInput('ng-maxlength', 'opts.maxlength');
-        //   _moveAttrToInput('ng-minlength', 'opts.minlength');
-        //   _moveAttrToInput('ng-pattern', 'opts.pattern');
-        //   _moveAttrToInput('ng-size', 'opts.size');
-        //
-        //   return {
-        //     post: function (scope, elem) {
-        //       $compile(elem)(scope);
-        //     }
-        //   };
-        // },
-        // controller: function () {
-        //
-        // },
-        link: function (scope, element) {
-          // scope.opts = scope.opts || {};
+        compile: function (tElement, tAttrs) {
+          var label = angular.element(tElement);
+          var input = label[0].children[0];
 
+          //label.removeAttr('ng-model');
+
+          function makeLoverCamelCase(str) {
+            var toUpperCase = function (a) {
+              return a.toUpperCase();
+            };
+            var toLowerCase = function (a) {
+              return a.toLowerCase();
+            };
+            var regex = /(?:^|\s)\S/g;
+
+            function makeUpperCase(s) {
+              return s.replace(regex, toUpperCase);
+            }
+
+            return str.split(/\-/).map(makeUpperCase).join('').replace(regex, toLowerCase);
+          }
+
+          function _moveAttrToInput(attr, val, attrNameAfter) {
+            if (tAttrs.hasOwnProperty(makeLoverCamelCase(attr))) {
+              //label.removeAttr(attr);
+              input.setAttribute(attrNameAfter || attr, val || tAttrs[makeLoverCamelCase(attr)]);
+            }
+          }
+
+          var attrList = [
+            'name',
+            'required',
+            'ng-required',
+            'disabled',
+            'ng-disabled',
+            'readonly',
+            'ng-readonly',
+            'minlength',
+            'ng-minlength',
+            'maxlength',
+            'ng-maxlength',
+            'size',
+            'ng-size'
+          ];
+
+          for (var i = 0; i < attrList.length; i++) {
+            _moveAttrToInput(attrList[i]);
+          }
+
+          _moveAttrToInput('jsvat-input-class', 'jsvatInputClassObj', 'ng-class');
+
+          return {
+            post: function (scope, elem) {
+              $compile(elem)(scope);
+            }
+          };
+        },
+        link: function (scope, element) {
           var invalid = '-invalid';
           var valid = '-valid';
 
